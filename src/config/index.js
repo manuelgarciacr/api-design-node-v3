32d44f9c4eadfc065b1d78f67c6bc26833318898
Manuel García Criado <manuel.garcia.cr@hotmail.com>
2022-03-21 13:57:26 +0100

    Lesson 1
     On branch es6-solution
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/config/dev.js
    	modified:   src/config/index.js
    	modified:   src/server.js
    	modified:   yarn.lock

--- src/config/index.js
+++ src/config/index.js
@@ -1,30 +1,34 @@
-import { merge } from 'lodash'
-const env = process.env.NODE_ENV || 'development'
+// import { merge } from 'lodash'
+import pkg from 'lodash';
+const { merge } = pkg;
+const env = process.env.NODE_ENV || 'development';
+import { config as devConfig } from './dev';
+import { config as testConfig } from './testing';
 
 const baseConfig = {
-  env,
-  isDev: env === 'development',
-  isTest: env === 'testing',
-  port: 3000,
-  secrets: {
-    jwt: process.env.JWT_SECRET,
-    jwtExp: '100d'
-  }
+    env,
+    isDev: env === 'development',
+    isTest: env === 'testing',
+    port: 3000,
+    secrets: {
+        jwt: process.env.JWT_SECRET,
+        jwtExp: '100d'
+    }
 }
 
 let envConfig = {}
 
 switch (env) {
-  case 'dev':
-  case 'development':
-    envConfig = require('./dev').config
-    break
-  case 'test':
-  case 'testing':
-    envConfig = require('./testing').config
-    break
-  default:
-    envConfig = require('./dev').config
+    case 'dev':
+    case 'development':
+        envConfig = devConfig;
+        break;
+    case 'test':
+    case 'testing':
+        envConfig = testConfig
+        break
+    default:
+        envConfig = devConfig;
 }
 
 export default merge(baseConfig, envConfig)
