b8a73c10029ca4dd260ca321ec2730c935ed2641
Manuel UOC <manuelgarciacr@uoc.edu>
2022-03-25 10:28:57 +0100

    Lesson 3
     On branch es6-solution
     Your branch is up to date with 'origin/es6-solution'.
    
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   src/config/dev.js
    	modified:   src/resources/item/item.model.js
    	modified:   src/utils/db.js
    	deleted:    yarn.lock

--- src/config/dev.js
+++ src/config/dev.js
@@ -3,4 +3,5 @@ export const config = {
         jwt: 'learneverything'
     },
     dbUrl: 'mongodb://localhost:27017/api-design'
+    // dbUrl: 'mongodb+srv://manuelgarciacr:<password>@cluster0.ornn8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
--- src/utils/db.js
+++ src/utils/db.js
@@ -7,3 +7,12 @@ export const connect = (url = options.dbUrl, opts = {}) => {
     { ...opts, useNewUrlParser: true }
   )
 }
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://manuelgarciacr:<password>@cluster0.ornn8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
